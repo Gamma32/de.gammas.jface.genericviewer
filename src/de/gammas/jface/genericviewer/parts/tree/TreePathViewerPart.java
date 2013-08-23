c6abfc0575b7eba8ba46a811df68d008e903fea7
Hendrik Still <hendrik.still@gammas.de>
2013-08-23 14:10:13 +0200

    Added functionality to the TreePathViewer

--- src/de/gammas/jface/genericviewer/parts/tree/TreePathViewerPart.java
+++ src/de/gammas/jface/genericviewer/parts/tree/TreePathViewerPart.java
@@ -1,10 +1,16 @@
 
 package de.gammas.jface.genericviewer.parts.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
+import org.eclipse.jface.viewers.ITreePathContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Composite;
 
 import de.gammas.jface.genericviewer.model.Person;
@@ -15,8 +21,47 @@ public class TreePathViewerPart {
 
 	@PostConstruct
 	public void postConstruct(Composite parent) {
-		TreeViewer treeViewer = new TreeViewer(parent);
-		treeViewer.setContentProvider(new MyTreePathContentProvider());
+		TreeViewer<Person,PersonRoot> treeViewer = new TreeViewer<Person,PersonRoot>(parent);
+		treeViewer.setContentProvider(new ITreePathContentProvider<Person,PersonRoot>(){
+
+			@Override
+			public void dispose() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void inputChanged(Viewer<PersonRoot> viewer,
+					PersonRoot oldInput, PersonRoot newInput) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public Person[] getElements(PersonRoot inputElement) {
+				Person[] persons = new Person[]{inputElement.getRootPerson()};
+				return persons;
+			}
+
+			@Override
+			public Person[] getChildren(TreePath<Person> parentPath) {
+				List<Person> childrenList = parentPath.getLastSegment().getChildren();
+				Person[] persons = new Person[childrenList.size()];
+				return childrenList.toArray(persons);
+			}
+
+			@Override
+			public boolean hasChildren(TreePath<Person> path) {
+				return (path.getLastSegment().getChildren() != null);
+			}
+
+			@Override
+			public TreePath<Person>[] getParents(Person element) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+		});
 		treeViewer.setLabelProvider(new MyLableProvider());
 		treeViewer.setInput(new PersonRoot());
 	}
